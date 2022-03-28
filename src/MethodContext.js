d87af468e42710fb4c81afc887c81c98d5efddce
alnmrkv <aln.mrkv@mail.ru>
2022-03-29 00:28:23 +0300

    Задание по контексту

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -6,10 +6,18 @@ import { EventEmitter } from './EventEmitter';
 2)В методе unsubscribe отпишитесь от события click с помощью EventEmitter.off(eventName, callback).
 В качестве callback нужно передавать тот же самый обработчик, который был передан при подписке.
  */
+let w = () => obj.count++;
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    func() {
+        count++;
+    },
+    subscribe() {
+        EventEmitter.on('click', w);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', w);
+    },
 };
 
 /*
@@ -19,7 +27,9 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        this.second(...args.reverse());
+    },
     second() {
         // здесь ничего писать не нужно
     },
